13920f7d07e7eb72a90ca9ac258598d2457bd209
Overpower90 <ioanvlad90@gmail.com>
2021-06-10 11:37:25 +0300

    gitignore added

--- mars_weight_calculator/target/rls/debug/deps/mars_weight_calculator-88dc6a4780ac4a42.d
+++ mars_weight_calculator/target/rls/debug/deps/mars_weight_calculator-88dc6a4780ac4a42.d
@@ -1,6 +1,6 @@
-/home/vlad/Desktop/rusty_boi/rust_dirt/mars_weight_calculator/target/rls/debug/deps/mars_weight_calculator-88dc6a4780ac4a42.rmeta: src/main.rs src/mars_weight_calculator.rs
+/home/vlad/Desktop/rusty_boi/rust_dirt/rust_projects/mars_weight_calculator/target/rls/debug/deps/mars_weight_calculator-88dc6a4780ac4a42.rmeta: src/main.rs src/mars_weight_calculator.rs
 
-/home/vlad/Desktop/rusty_boi/rust_dirt/mars_weight_calculator/target/rls/debug/deps/mars_weight_calculator-88dc6a4780ac4a42.d: src/main.rs src/mars_weight_calculator.rs
+/home/vlad/Desktop/rusty_boi/rust_dirt/rust_projects/mars_weight_calculator/target/rls/debug/deps/mars_weight_calculator-88dc6a4780ac4a42.d: src/main.rs src/mars_weight_calculator.rs
 
 src/main.rs:
 src/mars_weight_calculator.rs:
